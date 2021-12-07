73c823b8d0e531417c6ccb7b4a13e2be9b16e67c
el18126 <el18126@mail.ntua.gr>
2021-12-07 17:32:13 +0200

    shhh

--- Lunix/sshfs-script.sh
+++ Lunix/sshfs-script.sh
@@ -1,5 +1,5 @@
 #Will add command for sshfs - run it before utopia.sh
 
 sshfs -o allow_other user@10.0.2.2:/home/user/ask1 /home/user/host
-
+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
 #made changes to script
