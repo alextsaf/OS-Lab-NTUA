56cce251206f7bfe7956fe74aba233e582eb4ef4
el18126 <el18126@mail.ntua.gr>
2021-12-08 23:24:12 +0200

    added loop

--- Lunix/read_dev.c
+++ Lunix/read_dev.c
@@ -4,26 +4,33 @@
 #include <fcntl.h>
 //#include "lunix-chrdev.h"
 
+
 int main(int argc, char *argv[]){
   ssize_t read_bytes;
-  int buff[30];
+  int * buff;
   int fd;
 
-  fd = open(argv[1], O_RDONLY);
-  if (fd < 0){
-    perror("Error while opening device from userspace");
-    return 1;
-  }
+  while(1){
+    sleep(1);
+    buff = (int*) malloc(20);
+    fd = open(argv[1], O_RDONLY);
+    if (fd < 0){
+      perror("Error while opening device from userspace");
+      return 1;
+    }
 
 
-  read_bytes = read(fd,buff,1);
-
-  while(read_bytes > 0){
-    printf("%ls",buff);
     read_bytes = read(fd,buff,1);
-    if (read_bytes == -1){
-      perror("Error while reading buffer from userspace");
-      exit(0);
+
+    while(read_bytes > 0){
+      printf("%ls",buff);
+      read_bytes = read(fd,buff,1);
+      if (read_bytes == -1){
+        perror("Error while reading buffer from userspace");
+        exit(0);
+    }
+    }
+    close(fd);
   }
-  }
+
 }
