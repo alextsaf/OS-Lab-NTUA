7d4a6c4d8778ecb89f0083ab9d41ce38d798044a
bvrettos <billvrettos2000@gmail.com>
2021-12-09 09:47:54 +0200

    added mmap_read.c test file

---
+++ Lunix/mmap_read.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <inttypes.h>
+
+//directly from lunix.h
+struct lunix_msr_data_struct {
+  uint32_t magic;
+	uint32_t last_update;
+	uint32_t values[];
+
+};
+
+int main(int argc, char *argv[]){
+  int fd;
+  struct lunix_msr_data_struct *buff_a;
+  unsigned int buf_timestamp, current_timestamp;
+  current_timestamp = 0;
+
+  printf("Sensor Filename:%s\n", argv[1]);
+  if (argc != 2){
+    perror("Usage: ./mmap_read /dev/lunixXX-YYYY");
+    exit(1);
+  }
+
+  fd = open(argv[1], O_RDONLY);
+  if (fd < 0){
+    perror("Opening Sensor File failed");
+    exit(2);
+  }
+
+  buff_a = mmap(NULL, 30, PROT_READ, MAP_PRIVATE, fd, 0);
+  if (buff_a == MAP_FAILED){
+    perror("Memory mapping failed");
+    exit(0);
+  }
+
+  while(1) {
+    buf_timestamp = buff_a->last_update;
+    if(buf_timestamp != current_timestamp){
+      printf("Current Sensor Value: %u\n", buff_a->values[0]);
+    }
+  }
+  close(fd);
+  munmap(buff_a, 30);
+  }
