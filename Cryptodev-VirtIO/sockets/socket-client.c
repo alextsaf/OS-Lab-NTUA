1dcbc6f1dd1de8d7eea22a8cc5eb4c1c061464b7
Alexandros Tsafos <atsaf98@gmail.com>
2022-01-15 14:30:56 +0200

    1st commit for exc3
    Trying to debug sockets

--- Cryptodev-VirtIO/sockets/socket-client.c
+++ Cryptodev-VirtIO/sockets/socket-client.c
@@ -1,9 +1,9 @@
 /*
- * socket-client.c
- * Simple TCP/IP communication using sockets
- *
- * Vangelis Koukis <vkoukis@cslab.ece.ntua.gr>
- */
+* socket-client.c
+* Simple TCP/IP communication using sockets
+*
+* Vangelis Koukis <vkoukis@cslab.ece.ntua.gr>
+*/
 
 #include <stdio.h>
 #include <errno.h>
@@ -17,6 +17,8 @@
 #include <sys/time.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/select.h>
+#include <poll.h>
 
 #include <arpa/inet.h>
 #include <netinet/in.h>
@@ -28,13 +30,13 @@ ssize_t insist_write(int fd, const void *buf, size_t cnt)
 {
 	ssize_t ret;
 	size_t orig_cnt = cnt;
-	
+
 	while (cnt > 0) {
-	        ret = write(fd, buf, cnt);
-	        if (ret < 0)
-	                return ret;
-	        buf += ret;
-	        cnt -= ret;
+		ret = write(fd, buf, cnt);
+		if (ret < 0)
+		return ret;
+		buf += ret;
+		cnt -= ret;
 	}
 
 	return orig_cnt;
@@ -62,7 +64,7 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 	fprintf(stderr, "Created TCP socket\n");
-	
+
 	/* Look up remote hostname on DNS */
 	if ( !(hp = gethostbyname(hostname))) {
 		printf("DNS lookup failed for host %s\n", hostname);
@@ -89,36 +91,71 @@ int main(int argc, char *argv[])
 		perror("write");
 		exit(1);
 	}
-	fprintf(stdout, "I said:\n%s\nRemote says:\n", buf);
+	// fprintf(stdout, "I said:\n%s\nRemote says:\n", buf);
 	fflush(stdout);
 
 	/*
-	 * Let the remote know we're not going to write anything else.
-	 * Try removing the shutdown() call and see what happens.
-	 */
-	if (shutdown(sd, SHUT_WR) < 0) {
-		perror("shutdown");
-		exit(1);
-	}
+	* Let the remote know we're not going to write anything else.
+	* Try removing the shutdown() call and see what happens.
+	*/
+	// if (shutdown(sd, SHUT_WR) < 0) {
+	// 	perror("shutdown");
+	// 	exit(1);
+	// }
 
 	/* Read answer and write it to standard output */
+	//nfds = 2 (stdout and sd)
+	//readfds = 0(stdout) sd(sd)
+
+	struct timeval timeout;
+	fd_set readfds;
+	timeout.tv_sec = 0;
+	timeout.tv_usec = 0;
+
+
 	for (;;) {
-		n = read(sd, buf, sizeof(buf));
+		//readfds set init
+		FD_ZERO(&readfds);
 
-		if (n < 0) {
-			perror("read");
-			exit(1);
+		//add the conflicting fd's
+		FD_SET(0, &readfds);
+		FD_SET(sd, &readfds);
+
+		select(2, &readfds, NULL, NULL, &timeout);
+
+		if (FD_ISSET(sd, &readfds)){
+			n = read(sd, buf, sizeof(buf));
+
+			if (n < 0) {
+				perror("Client read from peer");
+				exit(1);
+			}
+
+			if (n <= 0)
+				break;
+
+			if (insist_write(1, buf, n) != n) {
+				perror("Client wrote to stdout");
+				exit(1);
+			}
 		}
+		if (FD_ISSET(0, &readfds)){
+			n = read(0, buf, sizeof(buf));
 
-		if (n <= 0)
-			break;
+			if (n < 0) {
+				perror("Client read from itself");
+				exit(1);
+			}
 
-		if (insist_write(0, buf, n) != n) {
-			perror("write");
-			exit(1);
+			if (n <= 0)
+				break;
+
+			if (insist_write(sd, buf, n) != n) {
+				perror("Client wrote to peer");
+				exit(1);
+			}
 		}
 	}
-
 	fprintf(stderr, "\nDone.\n");
 	return 0;
 }
